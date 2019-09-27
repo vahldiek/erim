36b8d886f7f1afe5e88f31202a25d6d503a064ac
Anjo Vahldiek-Oberwagner <vahldiek@mpi-sws.org>
2019-09-28 00:00:26 +0200

    adding missing test cases

---
+++ src/binaryanalysis/test/inadxrstor2.c
@@ -0,0 +1,9 @@
+/*
+ * inadxrstor.c
+ *
+ */
+
+int main(int argc, char **argv) {
+
+  asm ("mov $0x3ed56bae0f996f8d, %rax");  
+}
---
+++ src/binaryanalysis/test/inadxrstor3.c
@@ -0,0 +1,10 @@
+/*
+ * inadxrstor.c
+ *
+ */
+
+int main(int argc, char **argv) {
+
+  asm ("mov $0x69ae0f, %esi");
+
+}
